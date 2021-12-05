db7b69a7cd1bb6099368cd038df7c64e9c313819
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-05 16:56:21 +0300

    This commit adds an images model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
+from django.contrib.auth.models import User
+from django.db.models.fields import related
 
 # Create your models here.
+class Image(models.Model):
+    # title field
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=50)
+    caption = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True,null=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_images(self):
+        self.save()
+
+        # delete image
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_images_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
+
+
+    def update_images(self, title, caption):
+        self.title = title
+        self.caption = caption
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_images(cls):
+        today = dt.date.today()
+        images = Image.objects.all(post_date__date = today)
+        return images
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from insta.models import Image
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    image = Image.objects.all().order_by('-id')
+
+    return render(request, 'all-photos/index.html',{'image': image})
