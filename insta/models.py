53b62370c91839f802086a06ab3b28f70362d8ba
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-05 14:28:05 +0300

    This commit adds the templates folder and files

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import related
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create your models here
+class Images(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=50)
+    caption = models.TextField(max_length=2000)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+    likes_count = models.IntegerField(default=0)
+    comm_count = models.IntegerField(default=0)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='image',null=True)
+    def __str__(self):
+        return self.name
+
+    def update_caption(self, new_caption):
+        self.image_caption = new_caption
+        self.save()
+
+    @classmethod
+    def get_images_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def search_by_image_name(cls, search_term):
+        images = cls.objects.filter(
+            image_name__icontains=search_term)
+        return images
+    
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Images,Profile,Likes,Comments
+from django.contrib.auth.decorators import login_required
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    image = Images.objects.all().order_by('-id')
+    return render(request, 'instapp/index.html',{'image':image})
