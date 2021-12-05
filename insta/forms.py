a51ae7aa2ab6709eb0520d661ef05bd55d372c48
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-06 00:58:58 +0300

    This commit adds a profile page

---
+++ insta/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Image,Profile, Comments
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['likes','profile','posted_time','profile','user']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image','user']
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
