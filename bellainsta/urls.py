09aaac058d818f68a396d6abbd516869613321f3
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-07 23:03:24 +0300

    This commit adds a procfile

--- bellainsta/urls.py
+++ bellainsta/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth.views import  LogoutView
-
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('insta.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-     url(r'^logout/$',LogoutView, {"next_page": '/'})
+    url(r'^logout/$',LogoutView, {"next_page": '/'})
+
 ]
--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -18,3 +17,4 @@ urlpatterns=[
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
