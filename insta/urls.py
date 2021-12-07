cb09e77f477a5eb75b5bf3fd775f0932ebd3af1a
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-08 01:41:40 +0300

    This commit styles the navbar

--- insta/urls.py
+++ insta/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -17,4 +18,3 @@ urlpatterns=[
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
