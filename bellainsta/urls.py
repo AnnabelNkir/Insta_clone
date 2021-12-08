4060ed5b0a5462c830b55f6a48d4142bd55b43bb
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-08 10:56:53 +0300

    This commit deploys to heroku

--- bellainsta/urls.py
+++ bellainsta/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth.views import  LogoutView
+from django.contrib.auth import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('insta.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$',LogoutView, {"next_page": '/'})
-
 ]
