17c1c7eca58064e26ce660da6c1f17ada6817b76
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-08 17:58:50 +0300

    This commit deploys to heroku

--- bellainsta/urls.py
+++ bellainsta/urls.py
@@ -23,7 +23,7 @@ from insta.views import activate
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='/'),
+    path('accounts/signup/', RegistrationView.as_view(success_url='/'),
          name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
