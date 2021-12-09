842f6df08f057298e7431f95da515316c2e38b5d
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-09 21:13:02 +0300

    Debbuging

--- insta/views.py
+++ insta/views.py
@@ -68,7 +68,7 @@ def activate(request, uidb64, token):
 
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/accounts/register')
 def index(request):
     date = dt.date.today()
     photos = Image.objects.all()
