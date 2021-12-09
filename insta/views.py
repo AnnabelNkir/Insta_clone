b7323b91f7c0029726b48183fe766ab0cedd3de3
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-09 06:13:31 +0300

    Fixing bugs

--- insta/views.py
+++ insta/views.py
@@ -51,19 +51,8 @@ def signup(request):
 
 
 def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can <a href="/accounts/login/">Login</a>  your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+    context = {'uidb64':uidb64, 'token':token}
+    return render(request, 'templates/acc_active_email.html', context)
 
 def home(request):
     date = dt.date.today()
