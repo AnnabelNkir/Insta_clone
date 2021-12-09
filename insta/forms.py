b810eaaebcd2b1512fa6d907b3bae479423dfb3d
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-09 13:38:12 +0300

    Bug fixing

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile, Comments,Likes 
+from .models import Image,Profile, Comments
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
