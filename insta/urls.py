650cf6b719f94f9511fc0560b82985ec70ef929c
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-09 07:39:10 +0300

    Bug fixing

--- insta/urls.py
+++ insta/urls.py
@@ -11,9 +11,8 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
     url(r'^profiles/(\d+)',views.profiles,name='profiles'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
-        views.activate, name='activate'),
+    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
+    #     views.activate, name='activate'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
